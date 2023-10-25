94482e0f57aa7a4b24b9824fdc69193fef0dfc9c
Roman <compcc@ya.ru>
2023-10-25 19:58:08 +0300

    add Sem11

---
+++ untitled/src/Sem10/rpository/UserRepository.java
@@ -0,0 +1,10 @@
+package Sem10.rpository;
+import java.util.List;
+
+public interface UserRepository {
+    User getById(int id);
+    List<User> getAll();
+    void add(User user);
+    void update(User user);
+    void delete(int id);
+}
